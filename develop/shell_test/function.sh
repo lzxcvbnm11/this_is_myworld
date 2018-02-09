44c125c3133565c0c9d25ff14b43d24b56a3a4c9
leizhipeng <leizhipeng@sunniwell.net>
2018-02-09 12:07:45 +0800

    function.sh 的正确调用SVNGitcommit里的函数

--- develop/shell_test/function.sh
+++ develop/shell_test/function.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
-
+. SvnGitCommit
 DIR="./"
 FILE=
 #####Import Custom Functions
-. SvnGitCommit
 
-	Copy_To_FileSever "aa" "bb" "cc"
-	Calc_md5sum_shalsum
+	Copy_to_FileServer "aa" "bb" "cc"
+	Calc_md5sum
 	DIR=$(pwd)
 	FILE=$(ls)
-echo "ls = ${FILE}"
-echo "this is pwd = ${DIR}"
 echo "Ending ...."
